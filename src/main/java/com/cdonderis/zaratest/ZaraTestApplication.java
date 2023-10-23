a21c58481cb550bb918d9bb76831d009f89591ab
Carlos Donderis <cdonderis.design@gmail.com>
2023-10-23 14:39:28 +0200

    *Starter with all BBDD inserts.

--- src/main/java/com/cdonderis/zaratest/ZaraTestApplication.java
+++ src/main/java/com/cdonderis/zaratest/ZaraTestApplication.java
@@ -1,13 +1,19 @@
 package com.cdonderis.zaratest;
 
+import com.cdonderis.zaratest.repository.PriceRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ZaraTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZaraTestApplication.class, args);
-	}
 
+		ApplicationContext context = SpringApplication.run(ZaraTestApplication.class, args);
+
+		PriceRepository priceRepository = context.getBean(PriceRepository.class);
+
+		System.out.println("Precios registrados: " + priceRepository.findAll().size() + "\n" + priceRepository.findAll());
+	}
 }
