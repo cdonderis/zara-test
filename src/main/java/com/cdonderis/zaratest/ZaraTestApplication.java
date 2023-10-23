d8f23cd3a0409320b671160de8bf93de5f60bcd5
Carlos Donderis <cdonderis.design@gmail.com>
2023-10-23 13:45:47 +0200

    first commit

---
+++ src/main/java/com/cdonderis/zaratest/ZaraTestApplication.java
@@ -0,0 +1,13 @@
+package com.cdonderis.zaratest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdonderis/zaratest/ZaraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdonderis.zaratest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
